6c2f2c2f250b8f4da05dcdd2510d620f9c521d56
cinvenz <vincenzobortune@gmail.com>
2022-12-01 19:06:24 +0100

    fix  row card style

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,25 +1,33 @@
 <template>
-  <div class="row row-cols-5 g-4 container-cards py-5">
-    <h2 v-if="arrMovies.length > 0">Movies</h2>
-    <CardTvSeries
-      v-for="movie in arrMovies"
-      :key="movie.id"
-      :movie="movie"
-      :name="movie.title"
-      :originallanguage="movie.original_language"
-      :originaltitle="movie.original_title"
-      :vote="movie.vote_average"
-    />
-    <h2 v-if="arrTv.length > 0">TV series</h2>
-    <CardTvSeries
-      v-for="movie in arrTv"
-      :key="movie.id"
-      :movie="movie"
-      :name="movie.name"
-      :originallanguage="movie.original_language"
-      :originaltitle="movie.original_name"
-      :vote="movie.vote_average"
-    />
+  <div>
+    <div>
+      <h2 v-if="arrMovies.length > 0">Movies</h2>
+      <div class="row row-cols-4 g-4 container-cards py-5">
+        <CardTvSeries
+          v-for="movie in arrMovies"
+          :key="movie.id"
+          :movie="movie"
+          :name="movie.title"
+          :originallanguage="movie.original_language"
+          :originaltitle="movie.original_title"
+          :vote="movie.vote_average"
+        />
+      </div>
+    </div>
+    <div>
+      <h2 v-if="arrTv.length > 0" class="series">TV series</h2>
+      <div class="row row-cols-4 g-4 container-cards py-5">
+        <CardTvSeries
+          v-for="movie in arrTv"
+          :key="movie.id"
+          :movie="movie"
+          :name="movie.name"
+          :originallanguage="movie.original_language"
+          :originaltitle="movie.original_name"
+          :vote="movie.vote_average"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -38,4 +46,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.serie {
+  display: block;
+}
+</style>
